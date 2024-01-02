a07aaea343ada3eb01632a785eb093fb9b02c630
Dương Đông <donglieu@MacBook-DongLieu.local>
2024-01-03 00:33:19 +0700

    ok

--- proto.go
+++ proto.go
@@ -1,7 +1,7 @@
 package math
 
 // customProtobufType defines the interface custom gogo proto types must implement
-// in order to be used as a "customtype" extension.
+// in order to be used as a "customtype" extension. test.
 //
 // ref: https://github.com/cosmos/gogoproto/blob/master/custom_types.md
 type customProtobufType interface {
